26faab90f9a075bf26a89515a8293f5ecd665a8d
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-02-11 17:01:33 +0600

    added routing for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import { RegisterPage } from './pages/RegisterPage';
 import AccountPage from './pages/AccountPage';
+import LandingPage from './pages/LandingPage';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/account" element={<AccountPage />} />
