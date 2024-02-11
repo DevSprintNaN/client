c46b817db01603928ee7bcd79e2d82d596d988b3
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-02-11 14:37:03 +0600

    basic account page added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //components import
 import LoginPage from "./pages/LoginPage";
 import { RegisterPage } from './pages/RegisterPage';
+import AccountPage from './pages/AccountPage';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<AccountPage />} />
         </Routes>
       </BrowserRouter>
     </>
