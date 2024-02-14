27b64e0b6b8a3ddb524844f2e01b2389903c263c
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-02-14 21:37:36 +0600

    added add file page

---
+++ src/features/view-project/pages/add-file.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import "../../../utils/prismImports"
+import 'prismjs/themes/prism.css';
+import Editor from 'react-simple-code-editor';
+import UserNavbar from '../../../components/UserNavbar';
+import { useViewProject } from '../hooks/useViewProject';
+import { useParams } from 'react-router-dom';
+import prismLanguages from '../../../utils/prismLanguages';
+import IonIcon from '@reacticons/ionicons';
+
+const AddFilePage = () => {
+    // const includedLanguages = ['javascript', 'python', 'css', 'clike', 'bash'];
+    const filteredLanguages = Object.keys(languages)
+        .filter(lang => prismLanguages.includes(lang))
+        .reduce((obj, key) => {
+            obj[key] = languages[key];
+            return obj;
+        }, {});
+    console.log(filteredLanguages)
+    const [input, setInput] = useState("");
+    const [disabled, setDisabled] = useState(false);
+    const [language, setLanguage] = useState(languages.javascript);
+
+    const { id } = useParams()
+    const { directories, handleDirectories, currentDirectory, reverse } = useViewProject(id);
+
+    const handleCancel = () => {
+        if (!disabled) {
+            setInput("");
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setDisabled(true);
+
+
+        setDisabled(false);
+    };
+
+    const handleLanguageChange = (e) => {
+        setLanguage(filteredLanguages[e.target.value]);
+    };
+
+    return (
+        <>
+            <div className="h-full bg-violet-100 min-h-screen">
+                <UserNavbar />
+                <form onSubmit={handleSubmit}>
+                    <div className="w-full p-2 bg-violet-200 text-white flex justify-start items-stretch flex-wrap">
+                        <div className='flex-grow mx-4 my-auto'>
+                            <input type="text" className="py-2 w-full text-black bg-white rounded-md mt-2 md:mt-0" value={"\t" + currentDirectory} disabled={true}></input>
+
+                        </div>
+                        <div className='flex justify-end my-auto'>
+                            <button className=" bg-purple-700 text-white px-2 rounded-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center justify-center text-center transition-colors duration-300 me-1" onClick={() => { }}><IonIcon className="text-xl font-bold" name="clipboard-outline" /></button>
+                            <button type="submit" className={`bg-purple-700 text-white py-2 px-4 rounded-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300  ${disabled ? "opacity-50" : ""}`} disabled={disabled}>Save Changes</button>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center bg-black bg-opacity-75 transition-opacity">
+                        <div className="relative bg-white text-left shadow-xl transition-all w-full h-full">
+
+                            <div className='p-4'>
+                                <div className="flex flex-col md:w-1/4">
+                                    <label htmlFor="languages" className="block text-sm font-medium text-gray-700">Select A Language</label>
+                                    <select
+                                        id="languages"
+                                        className="border border-gray-300 rounded-md p-2 w-full mb-4"
+                                        value={Object.keys(filteredLanguages).find(key => filteredLanguages[key] === language)} // Find the key of the current language
+                                        onChange={handleLanguageChange}
+                                    >
+                                        {Object.keys(filteredLanguages).map((lang, index) => (
+                                            <option key={index} value={lang}>
+                                                {lang}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className='bg-violet-100 text-black min-h-[50vh] text-lg'>
+                                    <Editor
+                                        value={input}
+                                        onValueChange={setInput}
+                                        highlight={(code) => highlight(code, language)}
+                                        padding={10}
+                                        style={{
+                                            fontFamily: 'monospace',
+                                        }}
+                                        textareaClassName='min-h-[50vh] rounded-md border-0 focus:outline-none'
+                                        preClassName='min-h-[50vh] rounded-md border-0 focus:outline-none'
+                                    />
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default AddFilePage;
--- src/features/view-project/pages/view-project.jsx
+++ src/features/view-project/pages/view-project.jsx
@@ -1,31 +1,43 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import UserNavbar from '../../../components/UserNavbar';
 import Chat from '../../chat/components/chat';
 import Files from '../components/files';
 import { useViewProject } from '../hooks/useViewProject';
 import IonIcon from "@reacticons/ionicons";
 import { useState } from 'react';
-import FileUploadModal from '../components/add-files-modal';
-import FolderUploadModal from '../components/add-folder-modal';
-
+import FileUploadModal from '../components/upload-files-modal';
+import FolderUploadModal from '../components/upload-folder-modal';
 
 const ViewProject = () => {
     const { id } = useParams();
+    const navigate=useNavigate();
     const { directories, handleDirectories, currentDirectory, reverse } = useViewProject(id);
-    const [showFileUpload, setShowFileUpload]=useState(false)
-    const [showFolderUpload, setShowFolderUpload]=useState(false)
+    const [showFileUpload, setShowFileUpload] = useState(false);
+    const [showFolderUpload, setShowFolderUpload] = useState(false);
+
     return (
         <div className="h-full p-2 bg-violet-100 min-h-screen">
             <UserNavbar />
             {showFileUpload && (<FileUploadModal show={showFileUpload} setShow={setShowFileUpload} />)}
             {showFolderUpload && (<FolderUploadModal show={showFolderUpload} setShow={setShowFolderUpload} />)}
-            <div className="w-full p-2 flex bg-violet-200 text-white">
-                <button className="bg-purple-700 text-white px-2 py-2 rounded-md hover:bg-purple-900  focus:outline-none focus:ring-2 focus:ring-offset-2  transition-colors duration-300 me-5" onClick={() => reverse()}><IonIcon name="return-up-back-outline" className="me-2" />Go Back</button>
-                <button className="bg-purple-700 text-white px-2 py-2 rounded-md hover:bg-purple-900  focus:outline-none focus:ring-2 focus:ring-offset-2  transition-colors duration-300 me-5" onClick={() => setShowFileUpload(true)}><IonIcon name="document-outline" /><IonIcon name="add-outline" className="me-2" />New File</button>
-                <button className="bg-purple-700 text-white px-2 py-2 rounded-md hover:bg-purple-900  focus:outline-none focus:ring-2 focus:ring-offset-2  transition-colors duration-300 me-5" onClick={() => setShowFolderUpload(true)}><IonIcon name="folder-outline" /><IonIcon name="add-outline" className="me-2" />New Folder</button>
-                <input type="text" className="w-7/12 text-black bg-white rounded-md ms-2" value={"\t" + currentDirectory} disabled={true}></input>
+
+            <div className="w-full p-2 bg-violet-200 text-white flex justify-start items-stretch flex-wrap">
+                <div className="flex flex-wrap">
+                    <button className="w-14 bg-purple-700 text-white px-2 py-3 rounded-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center justify-center text-center transition-colors duration-300 me-5" onClick={() => reverse()}><IonIcon className="text-md" name="return-up-back-outline"/></button>
+
+                    <button className="w-14 bg-purple-700 text-white px-2 py-3 rounded-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center justify-center text-center transition-colors duration-300 me-5" onClick={() => navigate(`/view-project/${id}/add-file`)}><IonIcon className="text-lg" name="create-outline"/></button>
+
+
+                    <button className="w-14 bg-purple-700 text-white px-2 py-3 rounded-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center justify-center text-center transition-colors duration-300 me-5" onClick={() => setShowFileUpload(true)}><IonIcon className="text-md" name="document-outline" /><IonIcon className="text-md" name="add-outline" /></button>
+
+                    
+                    <button className="w-14 bg-purple-700 text-white px-2 py-3 rounded-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center justify-center text-center transition-colors duration-300 me-5" onClick={() => setShowFolderUpload(true)}><IonIcon className="text-md" name="folder-outline" /><IonIcon className="text-md" name="add-outline" /></button>
+                </div>
+                <div className='flex-grow mx-4 my-0.5'>
+                    <input type="text" className="h-full w-full text-black bg-white rounded-md ms-2 mt-2 md:mt-0" value={"\t" + currentDirectory} disabled={true}></input>
+                </div>
             </div>
-            <Files directories={directories} handleDirectories={handleDirectories}/>
+            <Files directories={directories} handleDirectories={handleDirectories} />
             <Chat />
         </div>
     );
