a69da5938997d50f7e468f34113fc8ba38808bab
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-02-15 20:50:27 +0600

    logout bug fix

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,10 +4,9 @@ import {Navigate, useLocation} from "react-router-dom"
 const ProtectedRoute = ({children,authentication}) => {
     let location = useLocation();
     if(!authentication) {
-        return <Navigate to="/error404" state={{ from: location}} replace />
+        return <Navigate to="/" state={{ from: location}} replace />
     }
  return children
-
 };
 
 export default ProtectedRoute;
