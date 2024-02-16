6d98aaa0b1459057bbbba396abe8efba6ff0c18e
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-02-16 13:55:46 +0600

    added new skill input option

--- src/features/profile/pages/AccountPage.jsx
+++ src/features/profile/pages/AccountPage.jsx
@@ -4,7 +4,11 @@ import Loading from "../../../components/Loading";
 import formDispatch, { formStates } from '../../../context/dispatch/formStatus';
 import FormMessage from '../../../components/FormMessage';
 import { IoClose } from "react-icons/io5";
-
+import { IoMdAdd } from "react-icons/io";
+import useFetchOptions from '../hooks/useFetchOptions';
+import useForm from '../hooks/useForm';
+import NewSkillInput from '../components/new-skill-input';
+import DisplayInputs from '../components/display-inputs';
 
 const AccountPage = () => {
     const [editMode, setEditMode] = useState(false);
@@ -12,85 +16,9 @@ const AccountPage = () => {
     const [message, setMessage] = useState("");
     const [payload, setPayload] = useState(null);
     const [disabled, setDisabled] = useState(false);
-    const [options, setOptions] = useState([]);
-    const [currSelected, setCurrSelected] = useState(null);
-
-    const [formData, setFormData] = useState({
-        email: "",
-        username: "",
-        skills: [],
-        currentPassword: "",
-        newPassword: "",
-        confirmNewPassword: "",
-    });
-
-    useEffect(() => {
-        const fetchOptions = async () => {
-            try {
-                const optionsData = await [{ label: "skill 1" }, { label: "skill 2" }, { label: "skill 3" }, { label: "skill 4" }, { label: "skill 5" }];
-                setOptions(optionsData);
-            } catch (error) {
-                console.error("Error fetching options:", error);
-            }
-        };
-
-        fetchOptions();
-    }, []);
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-
-        if (name === 'skills') {
-            const selectedSkill = e.target.value;
-            const isSelected = formData.skills.includes(selectedSkill);
-
-            let updatedSkills;
-            if (!isSelected) {
-                updatedSkills = [...formData.skills, selectedSkill];
-            } else {
-                updatedSkills = formData.skills;
-            }
-            setFormData({ ...formData, skills: updatedSkills });
-            setCurrSelected(selectedSkill)
-        } else {
-            setFormData({ ...formData, [name]: value });
-        }
-
-        if (name === 'newPassword' || name === 'confirmNewPassword') {
-            const password = document.getElementById('newPassword').value;
-            const confirmPassword = document.getElementById('confirmNewPassword').value;
-
-            if (password.length < 8 || password.length > 20) {
-                formDispatch(formStates.invalid, setFormState, setPayload);
-                setMessage('Password must be between 8 and 20 characters long.');
-                setDisabled(true)
-                return;
-            }
-
-            if (!/[A-Z]/.test(password) || !/\d/.test(password)) {
-                formDispatch(formStates.invalid, setFormState, setPayload);
-                setMessage('Password must contain at least one uppercase letter and one numeric character.');
-                setDisabled(true)
-                return;
-            }
-
-            if (password === confirmPassword) {
-                setDisabled(false);
-                setMessage('');
-                formDispatch(formStates.default, setFormState, setPayload);
-            } else {
-                setDisabled(true);
-                formDispatch(formStates.invalid, setFormState, setPayload);
-                setMessage("Password and confirm password values don't match!");
-            }
-        }
-        console.log(formData)
-    };
-
-    const handleRemoveSkill = (skill) => {
-        setFormData({ ...formData, skills: formData.skills.filter((selectedSkill) => selectedSkill !== skill) });
-    };
-    
+    const [showSkillSet, setShowSkillSet] = useState(false);
+    const { options } = useFetchOptions(); //for fetching dropdown options
+    const { formData, handleInputChange, handleRemoveSkill, currSelected, newSkillInput, addNewSkill, handleRemoveNewSkill, skillMessage, setSkillMessage } = useForm(options); //for handling form changes
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -143,28 +71,36 @@ const AccountPage = () => {
                                     <input type="text" id="username" name="username" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2  transition-colors duration-300" value={formData.username} disabled={!editMode} onChange={handleInputChange} />
                                 </div>
                                 <div className="pb-6">
-                                        <label htmlFor="skills" className="block text-sm font-medium text-gray-700">Skills</label>
-                                        {editMode && (<select name="skills" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={currSelected} onChange={handleInputChange}>
+                                    <label htmlFor="skills" className="block text-sm font-medium text-gray-700">Skills</label>
+                                    {editMode && (<><div className='flex'>
+                                        <select name="skills" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={currSelected} onChange={handleInputChange}>
                                             <option disabled selected>Select your skills</option>
                                             {options?.map((option, index) => (
-                                                <option key={index} value={option.label}>{option.label}</option>
+                                                <option key={index} value={option}>{option}</option>
                                             ))}
-                                        </select>)}
-
-                                                <div className='flex justify-start items-start pt-1'>
-                                        {formData.skills.length>0? formData.skills.map((skill, index) => (
-                                            <div key={index} className="flex items-center bg-gray-200 rounded-full px-3 py-1 mb-2 mr-2">
-                                                <span className="text-sm mr-1">{skill}</span>
-                                                <IoClose className="text-gray-600 cursor-pointer" onClick={() => handleRemoveSkill(skill)} />
-                                            </div>
-                                        )) : (<> <label className="block text-sm font-medium text-gray-700">No skills added yet</label></>)}
-                                        </div>
+                                        </select>
                                     </div>
 
+
+
+                                        <DisplayInputs className="pb-6" data={formData.skills} message={' '} handleRemove={handleRemoveSkill} />
+
+                                        {!showSkillSet && (<span className="block text-sm font-medium text-gray-700">Don't see what you're searching for? <div className='text-purple-800 inline-block cursor-pointer ' onClick={()=>setShowSkillSet(true)}>Add New</div><IoMdAdd className=' text-purple-800  inline-block' /></span>)}
+
+                                        {showSkillSet && (<div className='pt-6'>
+                                            <div className='w-full'>
+                                                <NewSkillInput formData={formData} handleInputChange={handleInputChange} newSkillInput={newSkillInput} addNewSkill={addNewSkill} />
+                                            </div>
+                                            {skillMessage && (<span className="block text-sm font-medium text-gray-700">{skillMessage}</span>)}
+                                            <DisplayInputs data={formData.newSkills} message={'Add a new skill to see it here'} handleRemove={handleRemoveNewSkill} />
+
+                                        </div>)}
+                                    </>
+                                    )}
+
+                                </div>
+
                                 {editMode && (<>
-
-                                    
-
                                     <div className="pb-4">
                                         <label htmlFor="currentPassword" className="block text-sm font-medium text-gray-700"> Current Password</label>
                                         <input type="password" id="currentPassword" name="currentPassword" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors duration-300" value={formData.currentPassword} onChange={handleInputChange} />
