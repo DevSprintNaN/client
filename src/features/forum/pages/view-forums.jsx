00a974bd31d2b7e496ce54d7b3d28e8a6ada94a3
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-02-19 17:13:09 +0600

    Search Forum+Upvote

--- src/features/forum/pages/view-forums.jsx
+++ src/features/forum/pages/view-forums.jsx
@@ -9,17 +9,17 @@ import { useNavigate } from 'react-router';
 import Loading from '../../../components/Loading';
 
 const ViewForums = () => {
-    const { forums,loading } = useFetchForums();
-    const { currentPage, setCurrentPage, totalPages, getCurrentItems, handlePageChange } = usePagination(12, forums);
+    const { loading,handleSearch,searchItems,search,setSearch } = useFetchForums();
+    const { currentPage, setCurrentPage, totalPages, getCurrentItems, handlePageChange } = usePagination(12, searchItems);
     const navigate = useNavigate()
-    if(!loading){
+    if (!loading) {
         return (
             <div className={`h-full p-2 bg-violet-100 w-full min-h-screen`} >
                 <UserNavbar />
                 <div className="text-gray-900 pb-8 pr-0 pl-0 bg-white">
                     <div className="w-full pt-4 pr-5 pb-6 pl-5 mt-0 mr-auto mb-0 ml-auto space-y-5 sm:py-0 md:py-2 sm:space-y-8 md:space-y-16 max-w-7xl">
                         <div className="flex flex-col items-center sm:px-5 md:flex-row">
-                            
+
                             <div className="flex flex-col items-start justify-center w-full h-full pt-6 pr-0 pb-6 pl-0 mb-6 md:mb-0">
                                 <div className="flex flex-col items-start justify-center h-full space-y-3 transform md:pr-10 lg:pr-16 md:space-y-5">
                                     <a className="text-4xl font-bold leading-none lg:text-5xl xl:text-6xl text-purple-800">Share Your Brilliance</a>
@@ -31,13 +31,16 @@ const ViewForums = () => {
                                         </p>
                                         <div className='w-full flex flex-start justify-start xs:py-2 py-2 space-x-2 xs:space-x-4 pr-1'>
                                             <div>
-                                                <button className={`w-full bg-purple-700 text-white px-4 py-2 rounded-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors duration-300`} onClick={()=>navigate('/add-forum')}>Create Forum</button>
+                                                <button className={`w-full bg-purple-700 text-white px-4 py-2 rounded-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors duration-300`} onClick={() => navigate('/add-forum')}>Create Forum</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="w-full content-center mb-3 p-3 flex justify-center items-center">
+                        <input type="text" placeholder="Search forums" className="w-3/4 p-2 border rounded-md focus:border-gray-200 focus:outline-none transition-colors duration-300" value={search} onChange={handleSearch} />
                     </div>
                     <ForumCards forums={getCurrentItems()} />
                     <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={handlePageChange} />
@@ -46,9 +49,9 @@ const ViewForums = () => {
             </div>
         );
     }
-    else{
+    else {
         return (
-            <Loading/>
+            <Loading />
         )
     }
 }
